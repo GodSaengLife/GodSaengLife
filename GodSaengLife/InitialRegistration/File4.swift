d4442c678e4d38f41acd3e999a8697587f7bc63d
Hongsik Choi <playhong16@gmail.com>
2023-09-25 19:43:14 +0900

    [NEW] hongsik / 사용자 이미지 뷰 추가

--- GodSaengLife/InitialRegistration/File4.swift
+++ GodSaengLife/InitialRegistration/File4.swift
@@ -1,2 +1,15 @@
 
-import Foundation
+import UIKit
+
+final class RegistrationViewController: UIViewController {
+    private let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.image = UIImage(systemName: "person")
+        return iv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
