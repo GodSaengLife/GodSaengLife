707c3f4c7eb57cfd86ef8896f57417432554b319
Hongsik Choi <playhong16@gmail.com>
2023-09-25 19:51:37 +0900

    [UPDATE] hongsik / 사용자 이미지 뷰 오토레이아웃 및 테두리 설정

--- GodSaengLife/InitialRegistration/File4.swift
+++ GodSaengLife/InitialRegistration/File4.swift
@@ -5,11 +5,28 @@ final class RegistrationViewController: UIViewController {
     private let imageView: UIImageView = {
         let iv = UIImageView()
         iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.backgroundColor = .clear
         iv.image = UIImage(systemName: "person")
+        iv.layer.cornerRadius = 75
+        iv.layer.borderWidth = 1
+        iv.layer.borderColor = UIColor.black.cgColor
+        iv.layer.masksToBounds = true
         return iv
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(imageView)
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 150),
+            imageView.heightAnchor.constraint(equalToConstant: 150)
+        ])
     }
 }
