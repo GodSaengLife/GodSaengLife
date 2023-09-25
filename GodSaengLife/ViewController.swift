6e8efd9eb2c37b65bcd2a83e03be9bbabc86ddfd
Kong <rhd9228@gmail.com>
2023-09-25 15:53:21 +0900

    Initial Commit

---
+++ GodSaengLife/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GodSaengLife
+//
+//  Created by t2023-m0078 on 2023/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .lightGray
+        print("테스트 - 로드 done")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
