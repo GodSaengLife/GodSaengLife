0d89fad980ba486f274d885acabbb4e40889b850
Hongsik Choi <playhong16@gmail.com>
2023-09-25 21:11:42 +0900

    [NEW]: hongsik / TimeSettingViewController 생성

---
+++ GodSaengLife/TimeSettingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TimeSettingViewController.swift
+//  GodSaengLife
+//
+//  Created by playhong on 2023/09/25.
+//
+
+import UIKit
+
+class TimeSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
