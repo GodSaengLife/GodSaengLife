74795a015a7b847814d8709cde67955ff2b9c08c
Hongsik Choi <playhong16@gmail.com>
2023-09-26 09:54:17 +0900

    [NEW]: hongsik / 피커뷰 기본 구현

--- GodSaengLife/TimeSettingViewController.swift
+++ GodSaengLife/TimeSettingViewController.swift
@@ -7,9 +7,77 @@
 
 import UIKit
 
-class TimeSettingViewController: UIViewController {
 
+final class TimeSettingViewController: UIViewController {
+    private var hour = [Int](0...23)
+    private var minute = [Int](0...59)
+    private var selectedHour: Int = 0
+    private var selectedMinute: Int = 0
+    private let timePicker: UIPickerView = {
+        let picker = UIPickerView()
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        return picker
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setConstraints()
     }
+    
+    private func configure(_ pickerView: UIPickerView) {
+        pickerView.dataSource = self
+        pickerView.delegate = self
+    }
+    
+    private func setConstraints() {
+        view.addSubview(timePicker)
+        NSLayoutConstraint.activate([
+            timePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            timePicker.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            timePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
+extension TimeSettingViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return hour.count
+        case 1:
+            return minute.count
+        default:
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return "\(hour)시간"
+        case 1:
+            return "\(minute)분"
+        default:
+            return ""
+            
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case 0:
+            selectedHour = hour[row]
+        case 1:
+            selectedMinute = minute[row]
+        default:
+            break
+        }
+    }
+}
+
+extension TimeSettingViewController: UIPickerViewDelegate {
+    
 }
--- GodSaengLife/ViewController.swift
+++ GodSaengLife/ViewController.swift
@@ -8,11 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let testButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("테스트", for: .normal)
+        button.titleLabel?.textColor = .black
+        button.addTarget(self, action: #selector(testButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func testButtonTapped() {
+        let moveVC = TimeSettingViewController()
+        let naviVC = UINavigationController(rootViewController: moveVC)
+        naviVC.modalPresentationStyle = .pageSheet
+        let pageSheet = naviVC.presentationController as? UISheetPresentationController
+        pageSheet?.detents = [.medium()]
+        pageSheet?.prefersGrabberVisible = true
+        pageSheet?.preferredCornerRadius = 8
+        pageSheet?.animateChanges { pageSheet?.selectedDetentIdentifier = .medium }
+        present(naviVC, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .white
+        view.addSubview(testButton)
+        testButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        testButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         // Do any additional setup after loading the view.
     }
 
