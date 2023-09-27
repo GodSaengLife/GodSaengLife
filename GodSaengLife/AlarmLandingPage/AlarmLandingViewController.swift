09872b8aa24dda6d3d3a8fc651fa503e3aaba6b6
JayBadass <jaybadass@naver.com>
2023-09-27 13:42:44 +0900

    [New]: 종범 / AlarmLandingVC 추가

---
+++ GodSaengLife/AlarmLandingPage/AlarmLandingViewController.swift
@@ -0,0 +1,44 @@
+
+import UIKit
+
+class AlarmLandingViewController: UIViewController {
+    
+    var alarmLandingView: AlarmLandingView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        alarmLandingView = AlarmLandingView(frame: view.bounds)
+        view.addSubview(alarmLandingView)
+        
+        alarmLandingView.quoteTextField.addTarget(self, action: #selector(checkMatchingText), for: .editingChanged)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return
+        }
+        let keyboardHeight = keyboardFrame.cgRectValue.height
+        view.frame.origin.y = -250
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        view.frame.origin.y = 0
+    }
+    
+    @objc func checkMatchingText() {
+        let isMatching = alarmLandingView.quoteLabel.text == alarmLandingView.quoteTextField.text
+        alarmLandingView.updateButtonAppearance(isEnabled: isMatching)
+    }
+}
+
+
