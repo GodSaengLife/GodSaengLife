5eb550cb255abbb67cd6162df4c4387fb500eefd
juyeon jeong <d9yarn@gmail.com>
2023-10-02 16:07:46 +0900

    [Fix] 주연 / 모델 재정의

--- GodSaengLife/MainPage/Model.swift
+++ GodSaengLife/MainPage/Model.swift
@@ -7,35 +7,8 @@
 
 import Foundation
 
-struct SetTheTime {
-    var wakeUp: Int
-    var exercise: Int
-    var study: Int
-}
-
-struct total {
-    var wakeUpSuccesseCount: Int
-    var exerciseTime: Int
-    var studyTime: Int
-}
-
 struct StopWatch {
-    var isStarted: Bool
-    var isCompleted: Bool
-    var counter: Double
-    var timer: Timer
-    let watchType: StopWatchType
-    
-    init(_ type: StopWatchType) {
-        self.isStarted = false
-        self.isCompleted = false
-        self.counter = 0.0
-        self.timer = Timer()
-        self.watchType = type
-    }
+var timer: Timer
+var counter: Double
 }
 
-enum StopWatchType {
-    case main
-    case total
-}
