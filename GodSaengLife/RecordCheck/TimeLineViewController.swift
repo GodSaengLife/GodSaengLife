ff60466aa467a9d2a8a4a1ea9ade8adbf972820d
Hongsik Choi <playhong16@gmail.com>
2023-10-06 09:47:22 +0900

    [UPDATE]: 홍식 / TimeLineView - 주연님 수정 사항 반영

--- GodSaengLife/RecordCheck/TimeLineViewController.swift
+++ GodSaengLife/RecordCheck/TimeLineViewController.swift
@@ -1,17 +1,20 @@
-
 import UIKit
 import SnapKit
 
 
 class TimeLineViewController: UIViewController {
     
+    //MARK: - Properties
+    
     var text:Int?
     var tableView = UITableView()
     var filteredData:[TimeLine]?
     
-    
-    
+    var previousSectionHeaderHeight: CGFloat = 0.0
+    var headerView: UIView?
 
+    
+    //MARK: - Life Cycle
     
     override func loadView() {
         view = tableView
@@ -20,30 +23,49 @@ class TimeLineViewController: UIViewController {
     override func viewDidLoad(){
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(named: "Main Color 2")
-        
         configure()
-        
-        
+        setBackgroundColor()
+        setTableViewAttribute()
     }
+    
     func reload(){
         self.tableView.reloadData()
     }
+    
+    func setBackgroundColor(){
+        view.backgroundColor = UIColor(named: "Main Color 2")
+    }
+    
+    func setTableViewAttribute(){
+        let topInset: CGFloat = 10
+        tableView.contentInset = UIEdgeInsets(top: topInset, left: 0, bottom: 0, right: 0)
+        tableView.allowsSelection = false
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = view.backgroundColor    }
 }
 
 
 extension TimeLineViewController {
     func configure() {
-                
         tableView.dataSource = self
         tableView.delegate = self
-        
-        
     }
 }
 
 extension TimeLineViewController: UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+         return 25
+     }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        if let headerView = view as? UITableViewHeaderFooterView {
+            headerView.textLabel?.textColor = UIColor.white // 원하는 색상으로 설정
+        }
+    }
 }
 
 extension TimeLineViewController: UITableViewDataSource {
@@ -55,7 +77,13 @@ extension TimeLineViewController: UITableViewDataSource {
         
         let cell = UITableViewCell()
         let label = UILabel()
+        label.text = "야호 기상미션 성공!"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        label.textAlignment = .left // 라벨 가운데 정렬
         
+        cell.contentView.addSubview(label)
+        cell.backgroundColor = tableView.backgroundColor
         
         let action = filteredData![indexPath.row]
         let date = action.date!
@@ -71,7 +99,9 @@ extension TimeLineViewController: UITableViewDataSource {
         default:
             kindString = "알람"
         }
+        
         var typeString = ""
+        
         if action.type == "start" {
             typeString = "시작"
         } else if action.type == "done" {
@@ -81,12 +111,7 @@ extension TimeLineViewController: UITableViewDataSource {
             typeString = "성공"
         }
         
-        
-        
-        
-        
-        label.text = kindString + " " + timeString + " " + typeString
-        
+//        label.text = kindString + " " + timeString + " " + typeString
         
         if action.second != -1 {
             label.text! += " \(action.second)"
@@ -95,23 +120,59 @@ extension TimeLineViewController: UITableViewDataSource {
         cell.addSubview(label)
         
         label.snp.makeConstraints{
-            
-            $0.center.equalToSuperview()
+            $0.left.equalToSuperview().offset(50) // 왼쪽으로 20 포인트 이동
+            $0.centerY.equalToSuperview()
         }
-        
+
         return cell
     }
     
     
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "알람"
+        case 1:
+            return "운동"
+        case 2:
+            return "공부"
+        default:
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear // 배경색을 투명하게 설정
+
+        let iconImageView = UIImageView(image: UIImage(systemName: "circle.fill"))
+        iconImageView.frame = CGRect(x: 25, y: 13, width: 12, height: 12)
+        iconImageView.tintColor = .white
+        headerView.addSubview(iconImageView)
+
+        // 섹션 타이틀 레이블을 추가합니다.
+        let titleLabel = UILabel()
+        titleLabel.text = self.tableView(tableView, titleForHeaderInSection: section)
+        titleLabel.textColor = UIColor.white
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 17)
+        titleLabel.frame = CGRect(x: 50, y: 0, width: tableView.frame.size.width - 50, height: 40) // 타이틀 레이블 위치 및 크기 조정
+        headerView.addSubview(titleLabel)
+
+        return headerView
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
-
-
-
-
-
-
-
-
-
-
-
